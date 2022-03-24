4ed2899895058861cd0641a3ea467850c653c04c
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2022-03-24 10:54:58 +0100

    test: add base test configuration

---
+++ tests/bootstrap.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2022 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+/**
+ * Test suite bootstrap for Plugin.
+ *
+ * This function is used to find the location of CakePHP whether CakePHP
+ * has been installed as a dependency of the plugin, or the plugin is itself
+ * installed as a dependency of an application.
+ */
+
+use Cake\Cache\Cache;
+use Cake\Cache\Engine\ArrayEngine;
+use Cake\Cache\Engine\NullEngine;
+use Cake\Core\Configure;
+use Cake\Datasource\ConnectionManager;
+use Cake\Log\Engine\ConsoleLog;
+use Cake\Log\Log;
+use Cake\ORM\TableRegistry;
+use Cake\Routing\Router;
+use Cake\Utility\Security;
+
+// Tokens introduced in PHP 7.4
+class PHP_Token_COALESCE_EQUAL extends PHP_Token
+{
+}
+
+class PHP_Token_FN extends PHP_Token
+{
+}
+
+$findRoot = function ($root) {
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+    throw new Exception('Cannot find the root of the application, unable to run tests');
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
+chdir($root);
+
+require_once 'vendor/cakephp/cakephp/src/basics.php';
+require_once 'vendor/autoload.php';
+
+define('ROOT', $root . DS . 'tests' . DS);
+define('APP', ROOT . 'TestApp' . DS);
+define('TMP', sys_get_temp_dir() . DS);
+define('LOGS', ROOT . DS . 'logs' . DS);
+define('CONFIG', ROOT . DS . 'config' . DS);
+define('CACHE', TMP . 'cache' . DS);
+define('CORE_PATH', $root . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
+
+Configure::write('debug', true);
+Configure::write('App', [
+    'encoding' => 'UTF-8',
+    'paths' => [
+        'plugins' => [ROOT . 'Plugin' . DS],
+        'templates' => [APP . 'Template' . DS],
+    ],
+]);
+
+Log::setConfig([
+    'debug' => [
+        'engine' => ConsoleLog::class,
+        'levels' => ['notice', 'info', 'debug'],
+    ],
+    'error' => [
+        'engine' => ConsoleLog::class,
+        'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+    ],
+]);
+
+Cache::drop('_bedita_object_types_');
+Cache::drop('_bedita_core_');
+Cache::setConfig([
+    '_cake_core_' => ['engine' => ArrayEngine::class],
+    '_cake_model_' => ['engine' => ArrayEngine::class],
+    '_bedita_object_types_' => ['className' => NullEngine::class],
+    '_bedita_core_' => ['className' => NullEngine::class],
+]);
+
+if (!getenv('db_dsn')) {
+    putenv('db_dsn=sqlite:///:memory:');
+}
+ConnectionManager::setConfig('test', [
+    'url' => getenv('db_dsn'),
+    // 'log' => true,
+]);
+ConnectionManager::alias('test', 'default');
+
+Router::reload();
+Security::setSalt('BEDITA');
+
+// clear all before running tests
+TableRegistry::getTableLocator()->clear();
+Cache::clear(false, '_cake_core_');
+Cache::clear(false, '_cake_model_');
+Cache::clear(false, '_bedita_object_types_');
